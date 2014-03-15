23e609aafbd17dcf03fe5642fac98375c13575d8
tkc49 <tkc49@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-15 08:02:47 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/caxias-hosoya/trunk@875803 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ caxias-hosoya.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package Caxias Hosoya
+ * @version 1.0
+ */
+/*
+Plugin Name: Caxias Hosoya
+Plugin URI: http://wordbench.org/groups/kobe/
+Description: これはただのプラグインではありません。伝説上のシンガー カシアス・ホソヤによって歌われた最も有名な WordPress のキーワードに要約される、同一世代のすべての人々の希望と情熱を象徴するものです。これは世界で最初で最後になるかわからない WordBench Kobe 公式プラグインです。このプラグインが有効にされると、プラグイン管理画面以外の管理パネルの右上に「君の瞳にダッシュボード」からの歌詞がランダムに表示されます。 
+Author: Caxias Hosoya
+Version: 1.0
+Author URI: https://twitter.com/tkc49
+*/
+
+function caxias_hosoya_get_lyric() {
+  /** These are the lyrics to Caxias Hosoya */
+	$lyrics = "眠れぬ夜は思い出すよ
+君のquery_posts　白くなった画面
+俺がバグを見つけて　君は笑ってくれたね
+その時から俺のメインクエリーは　君だけのものさ
+三日前から既読なのに　どうしてなんだベイべ〜♫
+（それLINEですやんほそやん）
+meta_queryが動かない
+夕暮れ時のRAINY DAYS
+君の笑顔にAccess
+404 Not foundで
+ピュアなハートが　クラウドクラッシュ
+君の瞳にダッシュボード
+溢れる想いがトラックバック
+そして届かない君からのコメント通知
+Wow〜 You are my Type〜";
+
+	// Here we split it into lines
+	$lyrics = explode( "\n", $lyrics );
+
+	// And then randomly choose a line
+	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+// This just echoes the chosen line, we'll position it later
+function caxias_hosoya() {
+	$chosen = caxias_hosoya_get_lyric();
+	echo "<p id='caxias'>$chosen</p>";
+}
+
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'caxias_hosoya' );
+
+// We need some CSS to position the paragraph
+function caxias_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
+	echo "
+	<style type='text/css'>
+	#caxias {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+		font-size: 11px;
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'caxias_css' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Caxias Hosoya ===
+Contributors: tkc49,gatespace,jim912
+Tags: 
+Requires at least: 1.0.0
+Tested up to: 3.8.1
+Stable tag: 3.8.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+これはただのプラグインではありません。伝説上のシンガー カシアス・ホソヤによって歌われた最も有名な WordPress のキーワードに要約される、同一世代のすべての人々の希望と情熱を象徴するものです。これは世界で最初で最後になるかわからない WordBench Kobe 公式プラグインです。このプラグインが有効にされると、プラグイン管理画面以外の管理パネルの右上に「君の瞳にダッシュボード」からの歌詞がランダムに表示されます。
+
+== Description ==
+
+これはただのプラグインではありません。伝説上のシンガー カシアス・ホソヤによって歌われた最も有名な WordPress のキーワードに要約される、同一世代のすべての人々の希望と情熱を象徴するものです。これは世界で最初で最後になるかわからない WordBench Kobe 公式プラグインです。このプラグインが有効にされると、プラグイン管理画面以外の管理パネルの右上に「君の瞳にダッシュボード」からの歌詞がランダムに表示されます。
+
+= Special Thanks =
+gatespace,jim912 and WordCamp Kobe2013 Member and WordPress Community friends.
+
+= Usage =
+
+[Read me.](https://github.com/tkc49/caxias-hosoya/blob/master/README.md)
+
+== Installation ==
+
+1. Upload the entire `caxias-hosoya` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+You will find an additional field while Edit categories.
+
+
+== Frequently Asked Questions ==
+
+You have no question because of simple plugin.
+
+
+== Changelog ==
+
+= 1.0.0 =
+First release.
+
+
